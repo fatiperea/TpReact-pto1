e3d14f47d72ff42e910d699f665e61974c528aab
fatiperea <fatimaperea3pppp0@gmail.com>
2024-01-31 22:37:34 -0300

    Limpio template y hago el punto 1

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Punto1 from "./components/Punto1";
+
+function App() {
+  return <Punto1></Punto1>
+}
+
+export default App;
---
+++ src/components/Punto1.jsx
@@ -0,0 +1,9 @@
+const Punto1 = () => {
+    return (
+        <h1>
+            Hello World
+        </h1>
+    );
+};
+
+export default Punto1;
